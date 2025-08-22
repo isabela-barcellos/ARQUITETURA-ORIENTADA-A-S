163ac881edc164387a3a00541e2e849a6f982fde
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-22 11:16:18 -0300

    Finaliza o conversor de números

--- src/main/java/br/com/fiap3espb/Application.java
+++ src/main/java/br/com/fiap3espb/Application.java
@@ -1,12 +1,16 @@
 package br.com.fiap3espb;
 
+import br.com.fiap3espb.stub_classes.NumberConversion;
+import br.com.fiap3espb.stub_classes.NumberConversionSoapType;
 import jakarta.xml.ws.Service;
 
 import javax.xml.namespace.QName;
+import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Application {
+    //conversor de números
     public static void main(String[] args) throws MalformedURLException {
         final String WSDL = "https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL";
         final String TARGET = "http://www.dataaccess.com/webservicesserver/";
@@ -15,6 +19,10 @@ public class Application {
         URL url = new URL(WSDL);
         QName qName = new QName(TARGET, NAME);
         Service service = Service.create(url, qName);
+
+        NumberConversionSoapType numberConversion = service.getPort(NumberConversionSoapType.class);
+        String convertedNumber = numberConversion.numberToWords(BigInteger.valueOf(0));
+        System.out.println(convertedNumber);
     }
 }
 
